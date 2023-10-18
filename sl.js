9e9ff1a54bddafac30c337b77c8f8a7f45cb6059
DidasMba <daniel.mbarushi@gmail.com>
2023-10-18 16:55:43 +0300

    DOM Trying

--- sl.js
+++ sl.js
@@ -1,14 +1,9 @@
-
-const slider = document.querySelector('.slider');
+/* const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slider li');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 
 let currentIndex = 0;
-
-function updateSlider() {
-  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
 
 function showSlide(index) {
   if (index < 0) {
@@ -18,8 +13,27 @@ function showSlide(index) {
   } else {
     currentIndex = index;
   }
-  updateSlider();
+
+  for (let i = 0; i < slides.length; i++) {
+    if (i === currentIndex) {
+      slides[i].style.opacity = 1;
+    } else {
+      slides[i].style.opacity = 0;
+    }
+  }
 }
 
 prevButton.addEventListener('click', () => showSlide(currentIndex - 1));
 nextButton.addEventListener('click', () => showSlide(currentIndex + 1));
+
+// Initialize by showing the first slide
+showSlide(currentIndex);
+
+
+
+*/
+
+
+
+const essaie = getElementsById("para");
+console.log("essaie");
